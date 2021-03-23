ccb59a1019908b72433cf0c4b79ee7a2718ddffe
KarlaDatu <Kajietu29@gmail.com>
2021-03-23 17:09:19 +0800

    2nd commit

--- CasestudyFinal.java
+++ CasestudyFinal.java
@@ -50,3 +50,53 @@ public void integers(){
         int b;
         System.out.println("Proceed to the next program");
       }
+// Part of Karla Datu
+  
+  public static void main(String[] args) {
+     CaseStudy objint = new CaseStudy();
+        objint.intro();
+        objint.Leader();
+           System.out.println("\n");
+           objint.groupmember();
+           objint.member1();
+           objint.member2();
+           objint.member3();
+           objint.member4();
+           objint.member5();
+
+        objint.integers();
+        objint.Reverseorder();
+        System.out.println("\n");
+      
+
+         Scanner sc = new Scanner(System.in);
+         objint.endpro();
+        System.out.println("Input two digit or more to see DIAMON:");
+        
+        int row = sc.nextInt();
+        sc.close();
+        CaseStudy d = new CaseStudy();
+        d.upperDiamond(row);
+        d.lowerDiamond(row - 2);
+    }
+    public void upperDiamond(int a) {
+        for (int i = 0; i < a; i++) {
+            for (int j = a - 1; j > i; j--)
+                System.out.print(" ");
+            for (int k = 0; k < 2 * i - 1; k++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+    }
+    public void lowerDiamond(int b) {
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j <= i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < 2 * (b - i) - 1; k++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+        System.out.println("Task Complete!");
+        System.out.println("End of the program. Thank you :)");      
+    }
+}
