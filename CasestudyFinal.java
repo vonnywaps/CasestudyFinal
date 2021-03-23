9c88604894bd8c8f105d0cfa1b7eacd1973df990
vonnywaps <tanggolicious@gmail.com>
2021-03-23 16:34:50 +0800

    Initial commit

---
+++ CasestudyFinal.java
@@ -0,0 +1,52 @@
+package casestudy;
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class CaseStudy {
+   
+// Part of Mark Jhon Von Leonen I
+public void integers(){
+       	 int c;
+        	for (c=1;c<=5;c++){
+            System.out.println(c);
+        }
+        System.out.println("-------------------------");
+    }
+    
+    public void Reverseorder(){
+        int d;
+        for (d=5;d>=0;d--){
+            System.out.println("A Reverse Order:"+d);
+        }
+    }
+    public void intro(){
+        int a;
+        System.out.println("Introducing our Group");
+          System.out.println("This is our simple program");
+       
+    }
+    public void Leader(){
+        int a;
+        System.out.println("Hi, I'm Mark Jhon Von Q. Leonen");
+          System.out.println("Leader in IPT2");
+       
+    }
+        public void groupmember(){
+        int a;
+        System.out.println("Team Member:");
+
+    }
+
+     public void member1(){
+        int b;
+        System.out.println("I'm Karla Datu");
+        System.out.println("From Guimba Nueva Ecija");
+           System.out.println("Part of this group");
+           System.out.println("\n");
+     
+    }
+
+      public void endpro(){
+        int b;
+        System.out.println("Proceed to the next program");
+      }
